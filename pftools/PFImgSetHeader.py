c1ab0060af8f31f42dc2a8f71d7c6d4945b63e5b
Lixin Zhan <lixinzhan@gmail.com>
2021-01-26 23:43:20 -0500

    added PFImgSetHeader and finished PFPatient using pydantic.

---
+++ pftools/PFImgSetHeader.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from datetime import datetime
+from typing import (List, Optional)
+from pydantic import BaseModel
+import logging
+from readPFile import readPFile
+
+class PFImgSetHeader(BaseModel):
+    x_dim: int
+    y_dim: int
+    z_dim: int
+    datatype: Optional[int]
+    bitpix: Optional[int]
+    bytes_pix: Optional[int]
+    x_pixdim: float
+    y_pixdim: float
+    z_pixdim: float
+    x_start: float
+    y_start: float
+    z_start: float
+    id: Optional[int]
+    patient_position = ''
+    binary_header_size: Optional[int]
+    manufacturer = ''
+    model = ''
+    couch_pos: Optional[float]
+    couch_height: Optional[float]
+    X_offset: Optional[float]
+    Y_offset: Optional[float]
+    dataset_modified = 0;
+    study_id: Optional[int]
+    exam_id: Optional[int]
+    patient_id = ''
+    modality = ''
+
+
+if __name__ == '__main__':
+    prjpath = os.path.dirname(os.path.abspath(__file__))+'/../'
+
+    FORMAT = "[%(asctime)s %(levelname)s - %(funcName)s] %(message)s"
+    logging.basicConfig(format=FORMAT, filename=prjpath+'logs/test.log', level=logging.INFO)
+
+    logging.info('Project foler is %s' % os.path.abspath(prjpath))
+
+    # Patient
+    Pdict = readPFile(prjpath+'examples/Patient_6204/ImageSet_0.header', 'ImageSet.header', 'dict')
+
+    pfImgSetHeader = PFImgSetHeader(**Pdict)
+
+    print(Pdict)
+
+    print(pfImgSetHeader.z_dim)
+    print(pfImgSetHeader.z_pixdim)
+    print(pfImgSetHeader.patient_id)
+    print(pfImgSetHeader.modality)
+
+
+ 
--- pftools/PFPatient.py
+++ pftools/PFPatient.py
@@ -1,6 +1,31 @@
 import os
 import sys
+from datetime import datetime
+from typing import (List, Optional)
 from pydantic import BaseModel
+import logging
+from readPFile import readPFile
+
+class _ImageSet(BaseModel):
+    ImageSetID: int
+    ImageName = ''
+    ExamID = ''
+    StudyID = ''
+    Modality = ''
+    NumberOfImages = 0
+    ScanTimeFromScanner = ''
+
+class _ImageSetList(BaseModel):
+    ImageSet: List[_ImageSet] = None
+
+class _Plan(BaseModel):
+    PlanID: int
+    PlanName = ''
+    PrimaryCTImageSetID = -1
+    PrimaryImageType = ''
+
+class _PlanList(BaseModel):
+    Plan: List[_Plan] = None
 
 class PFPatient(BaseModel):
     LastName = ''
@@ -12,20 +37,25 @@ class PFPatient(BaseModel):
     NextUniqueImageSetID = 0
     Gender = ''
     DateOfBirth = ''
-    ImageSetList: Optional[_ImageSet] = []
-    PlanList: Optional[_Plan] = []
+    ImageSetList: Optional[_ImageSetList] = None
+    PlanList: Optional[_PlanList] = None
 
-class _ImageSet(BaseModel):
-    ImageSetID: int
-    ImageName = ''
-    ExamID = ''
-    StudyID = ''
-    Modality = ''
-    NumberOfImages = 0
-    ScanTimeFromScanner: Optional[datetime] = None
 
-class __Plan(BaseModel):
-    PlanID: int
-    PlanName = ''
-    PrimaryCTImageSetID = -1
-    PrimaryImageType = ''
+if __name__ == '__main__':
+    prjpath = os.path.dirname(os.path.abspath(__file__))+'/../'
+
+    FORMAT = "[%(asctime)s %(levelname)s - %(funcName)s] %(message)s"
+    logging.basicConfig(format=FORMAT, filename=prjpath+'logs/test.log', level=logging.INFO)
+
+    logging.info('Project foler is %s' % os.path.abspath(prjpath))
+
+    # Patient
+    Pdict = readPFile(prjpath+'examples/Patient_6204/Patient', 'Patient', 'dict')
+    #print(Pdict)
+    #print(len(Pdict))
+    pfPatient = PFPatient(**Pdict)
+
+    print(pfPatient.MedicalRecordNumber)
+    print(pfPatient.ImageSetList.ImageSet[0].NumberOfImages)
+    print(pfPatient.ImageSetList.ImageSet[0].ScanTimeFromScanner)
+    print(pfPatient.PlanList.Plan[1].PlanName)
