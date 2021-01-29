c6cc3d07dd247e9c99793b53e473d32ff89fc6c5
Lixin Zhan <lixinzhan@gmail.com>
2021-01-29 14:45:55 -0500

    pytest for PFBackup added.

---
+++ tests/test_PFBackup.py
@@ -0,0 +1,34 @@
+import os
+import pytest
+import logging
+from pftools.PFBackup import PFBackup
+
+prjpath = os.path.dirname(os.path.abspath(__file__))+'/../'
+FORMAT = "[%(asctime)s %(levelname)s - %(funcName)s] %(message)s"
+logging.basicConfig(format=FORMAT, filename=prjpath+'logs/pytest.log', level=logging.WARNING)
+
+pfBackup = PFBackup(prjpath+'examples/Patient_6204')
+
+def test0_PFPFBackup():
+    assert(pfBackup.Patient.MedicalRecordNumber,
+            pfBackup.ImageSet[0].Header.z_dim,
+            pfBackup.ImageSet[0].ImageSetID,
+            pfBackup.ImageSet[0].ImageInfoList.ImageInfo[0].SliceNumber
+    ) == (
+        '00003030',
+        98,
+        0,
+        1
+    )
+
+def test1_PFBackup():
+    assert(pfBackup.Plan[0].PlanTrial.Trial.Name,
+            pfBackup.Plan[0].PlanID,
+            pfBackup.Plan[1].PlanTrial.Trial.Name,
+            pfBackup.Plan[1].PlanID
+    ) == (
+        'sMLC',
+        0,
+        'Direct Electron',
+        1
+    )
